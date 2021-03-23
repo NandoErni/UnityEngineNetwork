5f52c0d35806909d099b97d20737922f16e4b9c6
nandoerni <nando.erni@outlook.de>
2021-03-23 21:41:02 +0100

    removed constructor because it would fail everytime

--- UnityEngineNetwork/BaseNetworkManager.cs
+++ UnityEngineNetwork/BaseNetworkManager.cs
@@ -2,11 +2,6 @@
   /// <summary>Represents a network manager</summary>
   public abstract class BaseNetworkManager {
 
-    /// <summary>Creates a new network manager and initilizes the packet handlers</summary>
-    public BaseNetworkManager() {
-      InitPacketHandlers();
-    }
-
     /// <summary>Initilizes all packet handlers.</summary>
     protected abstract void InitPacketHandlers();
   }
